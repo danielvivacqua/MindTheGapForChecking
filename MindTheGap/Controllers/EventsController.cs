c7e77ef0b5f5a97e2f139e81bd1ed170b3afb5d9
danielvivacqua <drvivacqua@hotmail.com>
2017-09-08 16:56:22 -0400

    Initial commit

---
+++ MindTheGap/Controllers/EventsController.cs
@@ -0,0 +1,219 @@
+﻿using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+using MindTheGap.Models;
+using Newtonsoft.Json;
+using System;
+using System.Globalization;
+using System.Collections.Generic;
+
+namespace MindTheGap.Controllers
+{
+    public class EventsController : Controller
+    {
+        private MindTheGapEntities db = new MindTheGapEntities();
+        public string EventSummary { get; set; }
+        public string EventLocation { get; set; }
+        public string EventStart { get; set; }
+        public string EventEnd { get; set; }
+        public string EventDescription { get; set; }
+        public string EventRecurrence { get; set; }
+        public bool EventReminders { get; set; }
+        public string EventColor { get; set; }
+
+        //START OF TEST
+        [HttpPost]
+        public JsonResult GetUserEventInfo(EventsController events)
+        {
+
+            DateTime startDateResult;
+            DateTime endDateResult;
+
+
+            string summary = events.EventSummary;
+            string location = events.EventLocation;
+            string description = events.EventDescription;
+            string startTime = events.EventStart;
+
+            string endTime = events.EventEnd;
+            string recurrence = events.EventRecurrence;
+            bool reminders = events.EventReminders;
+            string color = events.EventColor;
+            string AllInfo;
+
+            //Event newEvent = new Event();
+            //newEvent.userId = "c03d4c0a-ee82-4980-ad00-bb4cb16f99ca";
+            //newEvent.summary = summary;
+            //newEvent.location = location;
+            //newEvent.description = description;
+            //DateTime.TryParse(startTime, out startDateResult);
+            //newEvent.starttime = startDateResult;
+            //DateTime.TryParse(endTime, out endDateResult);
+            //newEvent.endtime = endDateResult;
+            //newEvent.recurrence = recurrence;
+            //newEvent.reminders = reminders;
+            //newEvent.colorId = color;
+            //db.Events.Add(newEvent);
+            //db.SaveChanges();
+
+            if (startTime == "All Day")
+            {
+                AllInfo = summary + " is an All Day Event";
+            }
+            else
+            {
+                AllInfo = summary + " @ " + location + " From " + startTime + " to " + endTime;
+            }
+
+            return new JsonResult()
+            {
+
+            };
+
+            //return Json(summary, JsonRequestBehavior.AllowGet);
+        }
+        //END OF TEST
+
+        public JsonResult GetCalendarDatabase()
+        {
+            using (MindTheGapEntities db = new MindTheGapEntities())
+            {
+                var events = db.Events.ToList();
+                return new JsonResult { Data = events, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+            }
+        }
+        // GET: Events
+        public ActionResult Index()
+        {
+            var events = db.Events.Include(e => e.Interest).Include(e => e.AspNetUser);
+            return View(events.ToList());
+        }
+
+        //This is the action that's called when the gaps.cshmtl view is called
+        public ActionResult Gaps()
+        {
+            //This command executes the SQL query against the database
+            db.Database.ExecuteSqlCommand("SELECT* INTO GapsTable FROM (SELECT endtime GapStart, LEAD(starttime) OVER(ORDER BY starttime) GapEnd FROM[Event] WHERE starttime < endtime) AS Gaps");
+                
+            //We don't have anything we need to return to the view. We could replace this with a redirect action if we want.
+            return View();
+        }
+
+        // GET: Events/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Event @event = db.Events.Find(id);
+            if (@event == null)
+            {
+                return HttpNotFound();
+            }
+
+            
+
+            return View(@event);
+        }
+
+        // GET: Events/Create
+        public ActionResult Create()
+        {
+            ViewBag.interestId = new SelectList(db.Interests, "interestId", "userId");
+            ViewBag.userId = new SelectList(db.AspNetUsers, "Id", "Email");
+            return View();
+        }
+
+        // POST: Events/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "eventId,userId,interestId,summary,description,location,colorId,starttime,endtime,recurrence,reminders,completed")] Event @event)
+        {
+            if (ModelState.IsValid)
+            {
+
+                db.Events.Add(@event);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.interestId = new SelectList(db.Interests, "interestId", "userId", @event.interestId);
+            ViewBag.userId = new SelectList(db.AspNetUsers, "Id", "Email", @event.userId);
+            return View(@event);
+        }
+
+        // GET: Events/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Event @event = db.Events.Find(id);
+            if (@event == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.interestId = new SelectList(db.Interests, "interestId", "userId", @event.interestId);
+            ViewBag.userId = new SelectList(db.AspNetUsers, "Id", "Email", @event.userId);
+            return View(@event);
+        }
+
+        // POST: Events/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "eventId,userId,interestId,summary,description,location,colorId,starttime,endtime,recurrence,reminders,completed")] Event @event)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(@event).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.interestId = new SelectList(db.Interests, "interestId", "userId", @event.interestId);
+            ViewBag.userId = new SelectList(db.AspNetUsers, "Id", "Email", @event.userId);
+            return View(@event);
+        }
+
+        // GET: Events/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Event @event = db.Events.Find(id);
+            if (@event == null)
+            {
+                return HttpNotFound();
+            }
+            return View(@event);
+        }
+
+        // POST: Events/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Event @event = db.Events.Find(id);
+            db.Events.Remove(@event);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ MindTheGap/Controllers/GapsTablesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MindTheGap.Models;
+
+namespace MindTheGap.Controllers
+{
+    public class GapsTablesController : Controller
+    {
+        private MindTheGapEntities db = new MindTheGapEntities();
+
+        // GET: GapsTables
+        public ActionResult Index()
+        {
+            return View(db.GapsTables.ToList());
+        }
+
+        // GET: GapsTables/Details/5
+        public ActionResult Details(DateTime id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            GapsTable gapsTable = db.GapsTables.Find(id);
+            if (gapsTable == null)
+            {
+                return HttpNotFound();
+            }
+            return View(gapsTable);
+        }
+
+        // GET: GapsTables/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: GapsTables/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "GapStart,GapEnd")] GapsTable gapsTable)
+        {
+            if (ModelState.IsValid)
+            {
+                db.GapsTables.Add(gapsTable);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(gapsTable);
+        }
+
+        // GET: GapsTables/Edit/5
+        public ActionResult Edit(DateTime id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            GapsTable gapsTable = db.GapsTables.Find(id);
+            if (gapsTable == null)
+            {
+                return HttpNotFound();
+            }
+            return View(gapsTable);
+        }
+
+        // POST: GapsTables/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "GapStart,GapEnd")] GapsTable gapsTable)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(gapsTable).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(gapsTable);
+        }
+
+        // GET: GapsTables/Delete/5
+        public ActionResult Delete(DateTime id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            GapsTable gapsTable = db.GapsTables.Find(id);
+            if (gapsTable == null)
+            {
+                return HttpNotFound();
+            }
+            return View(gapsTable);
+        }
+
+        // POST: GapsTables/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(DateTime id)
+        {
+            GapsTable gapsTable = db.GapsTables.Find(id);
+            db.GapsTables.Remove(gapsTable);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
